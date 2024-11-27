bd559c3f6690ebe350ef49c0e91c79c4c51160eb
LeNguyenTuanMinh <Minh.LNT2414261@sis.hust.edu.vn>
2024-11-27 22:33:29 +0700

    Add files via upload

---
+++ bai_tap_13_2.cpp
@@ -0,0 +1,27 @@
+// 13.2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+double Power(double x, int n);
+int Factorial(int n);
+int Fibonacci(int t);
+int FindRoot(double a, double b, double c, double* x1, double* x2);
+void TemperatureConvert(double C, double* K, double* F);
+void Print(const int* a, int n);
+int Sum(const int* a, int n);
+double Average(const int* a, int n);
+double Calc(const double* a, int n, int x);
+int Parse(const char* s);
+char* VietName(char* s);
+int* FibonacciSequence(int n);
+int* SubArray(int* source, int n);
+char* ToUpper(const char* s);
+char* ToBin(int value);
+int* Histogram(const char* s);
+
+int main()
+{
+    while (true) { }
+}
